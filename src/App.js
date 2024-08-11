377ae39fe53aa4b6f780332ac5af7c38432619a8
KomfortKimko <komfortkimk@gmail.com>
2024-08-11 21:20:42 +0300

    jdd-testing-forms-codealong

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ function App() {
 
   const togglePepperoni = (e) => setPepperoniIsChecked(e.target.checked);
 
+  const [size, setSize] = useState("small");
+  const selectSize = (e) => setSize(e.target.value);
+
+  const [contactInfo, setContactInfo] = useState("");
+  const updateContactField = (e) => setContactInfo(e.target.value);
+
+  const [orderIsSubmitted, setOrderIsSubmitted] = useState(false);
+  const submitOrder = (e) => {
+  e.preventDefault();
+  setOrderIsSubmitted(true);
+  };
+
   return (
     <div>
       <h1>Place an Order</h1>
-      <form>
+      <p>
+        Your selection: {size} {pepperoniIsChecked ? "pepperoni" : "cheese"}
+      </p>
+      <form onSubmit={submitOrder}>
         <div>
           <h3>Toppings</h3>
           <input
@@ -20,7 +35,29 @@ function App() {
           />
           <label htmlFor="pepperoni">Add pepperoni</label>
         </div>
+        <div>
+          <h3>Size</h3>
+          <label htmlFor="select-size">Select size: </label>
+          <select id="select-size" value={size} onChange={selectSize}>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </div>
+        <div>
+          <h3>Contact Info</h3>
+          <label htmlFor="email">Enter your email address: </label>
+          <input
+            type="text"
+            value={contactInfo}
+            id="email"
+            placeholder="email address"
+            onChange={updateContactField}
+          />
+        </div>
+        <button type="submit">Submit Order</button>
       </form>
+      {orderIsSubmitted ? <h2>Thanks for your order!</h2> : null}
     </div>
   );
 }
